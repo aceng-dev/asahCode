1f0b1c856ed6abf6adbf07044f778c88b9d45c57
MarselDev <marselcristian10@gmail.com>
2026-09-22 09:48:09 +0800

    rubah raw

--- BelajarFE/event.js
+++ BelajarFE/event.js
@@ -1,28 +1,28 @@
-function welcome(){
-    alert('Deym bro muncul');
-    const mainContents = document.getElementById('mainContainer');
-    mainContents.style.display = 'inline';
+function welcome() {
+  alert("Deym bro muncul");
+  const mainContents = document.getElementById("mainContainer");
+  mainContents.style.display = "inline";
 }
 
-function increment(){
-    document.getElementById('count').innerText++;
-    
-    if(document.getElementById('count').innerText == 7){
-        const hiddenMessage = document.createElement('p');
-        hiddenMessage.innerText = 'Selamat! Anda menemukan hadiah tersembunyi...';
-        const image = document.createElement('img');
-        image.setAttribute(
-            'src',
-            'https://raw.githubusercontent.com/dicodingacademy/a315-web-pemula-labs/shared-files/catto.jpg',
-        );
-        const contents = document.getElementById('mainContainer');
-        contents.appendChild(hiddenMessage);
-        contents.appendChild(image);
-    }
+function increment() {
+  document.getElementById("count").innerText++;
+
+  if (document.getElementById("count").innerText == 7) {
+    const hiddenMessage = document.createElement("p");
+    hiddenMessage.innerText = "Selamat! Anda menemukan hadiah tersembunyi...";
+    const image = document.createElement("img");
+    image.setAttribute(
+      "src",
+      "https://raw.githubusercontent.com/aceng-dev/gambar-web_tugas1/fd908e573d55fe8651d178491fb12911f1c5f5e5/WhatsApp%20Image%202026-02-03%20at%2013.32.10.jpeg",
+    );
+    image.setAttribute("height", "250");
+    const contents = document.getElementById("mainContainer");
+    contents.appendChild(hiddenMessage);
+    contents.appendChild(image);
+  }
 }
 
+let incrementButton = document.getElementById("incrementButton");
+window.addEventListener("load", welcome);
 
-let incrementButton = document.getElementById('incrementButton');
-window.addEventListener('load',welcome);
-
-incrementButton.addEventListener('click', increment);
+incrementButton.addEventListener("click", increment);
